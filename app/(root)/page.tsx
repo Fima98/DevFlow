62cb98024e3b2cbe7142915422f85ffc35b9a3a9
Fima <loboda.fima@icloud.com>
2025-03-30 16:57:17 +0300

    Set up Auth Layouts, Social Auth Form, Next Auth, OAuth with GitHub amd Google providers

---
+++ app/(root)/page.tsx
@@ -0,0 +1,30 @@
+import { signOut } from "@/auth";
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <>
+      <h1 className="h1-bold text-primary-500">
+        Hello world i hope this is working
+      </h1>
+      <h1 className="h1-bold font-space-grotesk text-primary-500">
+        Hello world i hope this is working
+      </h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_UP });
+        }}
+      >
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
+};
+
+export default Home;
---
+++ auth.ts
@@ -0,0 +1,11 @@
+export const config = {
+  runtime: "nodejs",
+};
+
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub, Google],
+});
