b3c5cdf502bbe73408e2778b8e557573447f27b5
Fima <loboda.fima@icloud.com>
2025-04-01 18:26:02 +0300

    MobileNav and NavLinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,4 @@
-import { signOut } from "@/auth";
 import { auth } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   const session = await auth();
@@ -14,15 +11,6 @@ const Home = async () => {
       <h1 className="h1-bold font-space-grotesk text-primary-500">
         Hello world i hope this is working
       </h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_UP });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 };
