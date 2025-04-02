39b87c0b32686ca4448b38b65283064719a35183
Fima <loboda.fima@icloud.com>
2025-04-02 23:52:53 +0300

    Implement Local Search Bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,90 @@
-const Home = async () => {
+import Link from "next/link";
+
+import LocalSearch from "@/components/search/LocalSearch";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const questions = [
+  {
+    _id: "1",
+    title: "How to create a custom hook in React?",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: "2023-01-01",
+  },
+  {
+    _id: "2",
+    title: "What is the difference between props and state in React?",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: "2023-01-01",
+  },
+  {
+    _id: "3",
+    title: "How to handle form validation in React?",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    tags: [
+      { _id: "1", name: "react" },
+      { _id: "2", name: "javascript" },
+    ],
+    author: { _id: "1", name: "John Doe" },
+    upvotes: 10,
+    answers: 5,
+    views: 100,
+    createdAt: "2023-01-01",
+  },
+];
+
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLowerCase().includes(query?.toLowerCase())
+  );
+
   return (
     <>
-      <h1 className="h1-bold text-primary-500">
-        Hello world i hope this is working
-      </h1>
-      <h1 className="h1-bold font-space-grotesk text-primary-500">
-        Hello world i hope this is working
-      </h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All questions</h1>
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
+        </Button>
+      </section>
+      <div className="mt-11">
+        <LocalSearch
+          route="/"
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+      </div>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
     </>
   );
 };
--- components/navigation/navbar/Theme.tsx
+++ components/navigation/navbar/Theme.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
 import { useTheme } from "next-themes";
 import * as React from "react";
 
@@ -12,15 +12,27 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-const Theme = () => {
+const ThemeToggle = () => {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="ghost" size="icon">
+          <Image
+            src="/icons/sun.svg"
+            alt="Sun Icon"
+            width={20}
+            height={20}
+            className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Image
+            src="/icons/moon.svg"
+            alt="Moon Icon"
+            width={20}
+            height={20}
+            className="absolute rotate-90 scale-0 text-primary-500 transition-all dark:rotate-0 dark:scale-100"
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -39,4 +51,4 @@ const Theme = () => {
   );
 };
 
-export default Theme;
+export default ThemeToggle;
