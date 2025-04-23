f8496a37524b86c590ef5c3e97bd1f724a75da1d
Fima <loboda.fima@icloud.com>
2025-04-23 15:29:24 +0300

    Implement TagQuestions Action

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,10 +1,11 @@
 import { FilterQuery } from "mongoose";
 
-import { Tag } from "@/database";
+import { Question, Tag } from "@/database";
 
 import action from "../handlers/action";
 import handleError from "../handlers/error";
-import { PaginatedSearchSchema } from "../validations";
+import { NotFoundError } from "../http-errors";
+import { GetTagQuestionsSchema, PaginatedSearchSchema } from "../validations";
 
 export async function getTags(
   params: PaginatedSearchParams
@@ -53,11 +54,63 @@ export async function getTags(
       .sort(sortCriteria)
       .skip(skip)
       .limit(limit);
-    const isNext = totalTags > page * tags.length;
+    const isNext = totalTags > skip * tags.length;
     return {
       success: true,
       data: {
         tags: JSON.parse(JSON.stringify(tags)),
+        isNext,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function getTagQuestions(
+  params: GetTagQuestionsParams
+): Promise<
+  ActionResponse<{ tag: Tag; questions: Question[]; isNext: boolean }>
+> {
+  const validationResult = await action({
+    params,
+    schema: GetTagQuestionsSchema,
+  });
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { tagId, page = 1, pageSize = 10, query } = validationResult.params!;
+
+  const skip = (Number(page) - 1) * pageSize;
+  const limit = Number(pageSize);
+
+  try {
+    const tag = await Tag.findById(tagId);
+    if (!tag) throw new NotFoundError("Tag");
+    const filterQuery: FilterQuery<typeof Question> = {
+      tags: { $in: [tagId] },
+    };
+
+    if (query) {
+      filterQuery.title = { $regex: query, $options: "i" };
+    }
+    const totalQuestions = await Question.countDocuments(filterQuery);
+    const questions = await Question.find(filterQuery)
+      .select("_id title views answers upvotes downvotes author createdAt")
+      .populate([
+        { path: "author", select: "name avatar" },
+        { path: "tags", select: "name" },
+      ])
+      .lean()
+      .skip(skip)
+      .limit(limit);
+    const isNext = totalQuestions > skip * questions.length;
+    return {
+      success: true,
+      data: {
+        tag: JSON.parse(JSON.stringify(tag)),
+        questions: JSON.parse(JSON.stringify(questions)),
         isNext,
       },
     };
