56122ae307964059c79e5269bf5a7e327179550e
Fima <loboda.fima@icloud.com>
2025-05-01 17:40:20 +0300

    Implement hasVoted

--- lib/actions/vote.action.ts
+++ lib/actions/vote.action.ts
@@ -117,3 +117,43 @@ export async function createVote(
     session.endSession();
   }
 }
+
+export async function hasVoted(
+  params: HasVotedParams
+): Promise<ActionResponse<HasVotedResponse>> {
+  const validationResult = await action({
+    params,
+    schema: CreateVoteSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { targetId, targetType } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const vote = await Vote.findOne({
+      author: userId,
+      actionId: targetId,
+      actionType: targetType,
+    });
+    if (!vote) {
+      return {
+        success: true,
+        data: { hasUpVoted: false, hasDownVoted: false },
+      };
+    }
+    return {
+      success: true,
+      data: {
+        hasUpVoted: vote.voteType === "upvote",
+        hasDownVoted: vote.voteType === "downvote",
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
