7d877e751dcd4586a9747ffe79a149d498877838
Fima <loboda.fima@icloud.com>
2025-05-04 20:43:51 +0300

    Develop the collections page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import QuestionCard from "@/components/cards/QuestionCard";
+import DataRenderer from "@/components/DataRenderer";
+import CommonFilter from "@/components/filters/CommonFilter";
+import LocalSearch from "@/components/search/LocalSearch";
+import { CollectionFilters } from "@/constants/filters";
+import ROUTES from "@/constants/routes";
+import { EMPTY_QUESTION } from "@/constants/states";
+import { getSavedQuestions } from "@/lib/actions/collection.action";
 
-const Collections = () => {
-  return <div>Collections</div>;
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+const Collections = async ({ searchParams }: SearchParams) => {
+  const { page, pageSize, query = "", filter = "" } = await searchParams;
+
+  const { success, data, error } = await getSavedQuestions({
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) || 10,
+    query,
+    filter,
+  });
+
+  const { collections } = data || { questions: [] };
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Favorites</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearch
+          route={ROUTES.COLLECTION}
+          imgSrc="/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />
+        <CommonFilter
+          filters={CollectionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+      <DataRenderer<Collection>
+        success={success}
+        error={error}
+        data={collections}
+        empty={EMPTY_QUESTION}
+        render={(collections) => (
+          <div className="mt-10 flex w-full flex-col gap-6">
+            {collections.map((collection) => (
+              <QuestionCard
+                key={collection._id}
+                question={collection.question}
+              />
+            ))}
+          </div>
+        )}
+      />
+    </>
+  );
 };
 
 export default Collections;
